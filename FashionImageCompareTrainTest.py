eb2e5493016fc3329f849b740ef56e352ccd85b0
bKolisnik <bKolisnik@gmail.com>
2020-03-24 14:07:01 -0400

    Print

--- FashionImageCompareTrainTest.py
+++ FashionImageCompareTrainTest.py
@@ -74,7 +74,7 @@ test_generator = test_datagen.flow_from_dataframe(
         x_col="filepath",
         y_col=modelT,
         target_size=target_size,
-        batch_size=1,
+        batch_size=batch,
         class_mode='categorical')
 
 train_generator = test_datagen.flow_from_dataframe(
@@ -83,7 +83,7 @@ train_generator = test_datagen.flow_from_dataframe(
     x_col="filepath",
     y_col=modelT,
     target_size=target_size,
-    batch_size=1,
+    batch_size=batch,
     class_mode='categorical')
 val_generator = test_datagen.flow_from_dataframe(
     dataframe=val_df,
@@ -91,7 +91,7 @@ val_generator = test_datagen.flow_from_dataframe(
     x_col="filepath",
     y_col=modelT,
     target_size=target_size,
-    batch_size=1,
+    batch_size=batch,
     class_mode='categorical')
 
 
@@ -106,12 +106,12 @@ def save_predictions():
     np.save('train_pred_'+modelT+"_"+typ+'.npy',train_pred)
     '''
     STEP_SIZE_TEST = test_generator.n // test_generator.batch_size
-    test_pred = model.predict(x=test_generator)
+    test_pred = model.predict(x=test_generator,steps=STEP_SIZE_TEST)
 
     np.save('test_pred_'+modelT+"_"+typ+'.npy',test_pred)
 
     STEP_SIZE_VAL = val_generator.n // val_generator.batch_size
-    val_pred = model.predict(x=val_generator)
+    val_pred = model.predict(x=val_generator,steps=STEP_SIZE_VAL)
 
     np.save('val_pred_'+modelT+"_"+typ+'.npy', val_pred)
 
@@ -135,15 +135,20 @@ confusion_matrix(train_labels,train_pred)
 
 test_pred = np.load('test_pred_'+modelT+"_"+typ+'.npy')
 test_labels = test_generator.classes
+print("Test Labels: ")
+print(test_labels[:10])
 test_pred = np.argmax(test_pred, axis=-1)
+print("Test predictions: ")
+print(test_pred[:10])
 acc = sum(test_pred==test_labels)/len(test_pred)
 print("Testing Accuracy is "+str(acc))
 
 
 val_pred = np.load('val_pred_'+modelT+"_"+typ+'.npy')
 val_labels = val_generator.classes
-print(val_labels)
+print(val_labels[:10])
 val_pred = np.argmax(val_pred, axis=-1)
+print(val_pred[:10])
 np.savetxt("val_pred_max_"+typ+".txt",val_pred)
 acc = sum(val_pred==val_labels)/len(val_pred)
 print("Validation Accuracy is "+str(acc))
