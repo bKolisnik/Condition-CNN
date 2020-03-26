1a5ebffdc61b8c08fe536c83f3ae1cc0b167eb66
bKolisnik <bKolisnik@gmail.com>
2020-03-25 22:06:11 -0400

    shape

--- RecurrentTrain.py
+++ RecurrentTrain.py
@@ -161,7 +161,7 @@ def get_flow_from_dataframe(generator, dataframe,
 
         print(x_1[1][0])
         print(x_1[1][1])
-        yield [x_1[0], x_1[1][:,0], x_1[1][:,1]], x_1[1]
+        yield [x_1[0], x_1[1][0], x_1[1][1]], x_1[1]
 
         #should be list of length 3 and list of length 3
 
