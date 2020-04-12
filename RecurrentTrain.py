f13d9343ec5d242e9d0d8c86aa7d0a28ae8dfea6
bKolisnik <bKolisnik@gmail.com>
2020-04-12 13:53:22 -0400

    final changes

--- RecurrentTrain.py
+++ RecurrentTrain.py
@@ -7,7 +7,7 @@
 # This Python 3 environment comes with many helpful analytics libraries installed
 # It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
 # For example, here's several helpful packages to   in
-import json
+
 import numpy as np  # linear algebra
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
